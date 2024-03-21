87a8ac98a2d5ddab2643ea072374ce734cb969c0
Fisjkars <maxime.escourbiac@gmail.com>
2024-03-21 09:22:41 +0100

    Import Redscan-Archetype

---
+++ tools/Redscan-Archetype/src/main/resources/archetype-resources/src/main/java/com/michelin/cert/redscan/ScanApplication.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2023 Michelin CERT (https://cert.michelin.com/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.michelin.cert.redscan;
+
+import org.apache.logging.log4j.LogManager;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * RedScan scanner main class.
+ *
+ * @author Maxime ESCOURBIAC
+ * @author Maxence SCHMITT
+ * @author Axel REMACK
+ */
+@SpringBootApplication
+public class ScanApplication {
+
+  // Only required if pushing data to queues
+  private final RabbitTemplate rabbitTemplate;
+
+  // Only required if the worker need to cache result.
+  @Autowired
+  private CacheConfig cacheConfig;
+
+  /**
+   * Constructor to init rabbit template. Only required if pushing data to queues
+   *
+   * @param rabbitTemplate Rabbit template.
+   */
+  public ScanApplication(RabbitTemplate rabbitTemplate) {
+    this.rabbitTemplate = rabbitTemplate;
+  }
+
+  /**
+   * RedScan Main methods.
+   *
+   * @param args Application arguments.
+   */
+  public static void main(String[] args) {
+    SpringApplication.run(ScanApplication.class, args);
+  }
+
+  /**
+   * Message executor.
+   *
+   * @param message Message received.
+   */
+  @RabbitListener(queues = {RabbitMqConfig.QUEUE_DOMAINS})
+  public void receiveMessage(String message) {
+    try {
+      LogManager.getLogger(ScanApplication.class).info(String.format("Blah : %s", message));
+    } catch (Exception ex) {
+      LogManager.getLogger(ScanApplication.class).error(String.format("General Exception : %s", ex.getMessage()));
+    }
+  }
+
+}
