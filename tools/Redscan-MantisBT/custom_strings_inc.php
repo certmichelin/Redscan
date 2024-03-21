6c17d515e61521db23dd8cd98a85c1111d77b139
Fisjkars <maxime.escourbiac@gmail.com>
2024-03-21 09:31:30 +0100

    Import Redscan-MantisBT

---
+++ tools/Redscan-MantisBT/custom_strings_inc.php
@@ -0,0 +1,13 @@
+<?php
+# Translation for Custom Status Code: testing
+$s_status_enum_string = '10:new,40:confirmed,60:ongoing,70:toverify,90:closed';
+
+$s_ongoing_bug_title  = 'Mark the issue as being fixed';
+$s_ongoing_bug_button = 'Resolution on-going';
+$s_email_notification_title_for_status_bug_ongoing = 'The following issue is under fix.';
+
+$s_toverify_bug_title = 'Mark issue Ready for Testing';
+$s_toverify_bug_button = 'Ready for Testing';
+$s_email_notification_title_for_status_bug_toverify = 'The following issue is ready for TESTING.';
+
+?>
