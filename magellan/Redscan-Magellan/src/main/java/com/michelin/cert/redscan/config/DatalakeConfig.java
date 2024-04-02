f3c7bd73703bbfd1badf74f128ac4cb1cd39c746
Fisjkars <maxime.escourbiac@gmail.com>
2024-04-03 00:09:08 +0200

    Fix package name

--- magellan/Redscan-Magellan/src/main/java/com/michelin/cert/redscan/config/DatalakeConfig.java
+++ magellan/Redscan-Magellan/src/main/java/com/michelin/cert/redscan/config/DatalakeConfig.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.michelin.cert.redscan;
+package com.michelin.cert.redscan.config;
 
 import com.michelin.cert.redscan.utils.datalake.DatalakeStorage;
 
