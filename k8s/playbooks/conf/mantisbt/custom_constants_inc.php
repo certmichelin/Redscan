16f15c0f52cb1276c027b33e86e07c91846a6671
Maxime Escourbiac <maxime.escourbiac@michelin.com>
2024-07-31 17:49:05 +0200

    Install mantisbt

---
+++ k8s/playbooks/conf/mantisbt/custom_constants_inc.php
@@ -0,0 +1,13 @@
+<?php
+# Custom status code.
+define( 'ONGOING', 60 );
+define( 'TOVERIFY', 70 );
+
+# Custom priority.
+define( 'P1', 1 );
+define( 'P2', 2 );
+define( 'P3', 3 );
+define( 'P4', 4 );
+define( 'P5', 5 );
+
+?>
